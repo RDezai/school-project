bb6af56306c3d103e548d2ab4f088f8156231662
Illiyanna <angelofnightvision@gmail.com>
2023-11-22 14:51:48 +0100

    Projektphasen Klasse

---
+++ projectmanagement/projectmanagement/src/Projektphasen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectmanagement.src
+{
+    internal class Projektphasen
+    {
+        public int PhasID;
+        public string Kennung = "";
+        public string Bezeichnung = "";
+        public string ProjID = "";
+        public int Dauer;
+        public string Vorg = "";
+
+        public static Projektphasen GetDatabaseObject(SQLiteDataReader reader)
+        {
+            Projektphasen Phase = new Projektphasen();
+            Phase.PhasID = reader.GetInt32(0);
+            Phase.Kennung = reader.GetString(1);
+            Phase.Bezeichnung = reader.GetString(2);
+            Phase.ProjID = reader.GetString(3);
+            Phase.Dauer = reader.GetInt32(4);
+            Phase.Vorg = reader.GetString(5);
+
+            return Phase;
+        }
+
+        public static string GetTableName()
+        {
+            return "Projektphase";
+        }
+
+        public override string ToString()
+        {
+            return $"Phase: (ID: {PhasID}, Kenn: {Kennung}, Bez: {Bezeichnung}, Proj {ProjID}, Dauer {Dauer}, Vorg {Vorg})\n";
+        }
+    }
+}
